ddd9aad2e1a4da7f0a71408970bab4ecc7d38bf9
unknown <sayadiademali335@gmail.com>
2025-08-11 13:57:55 +0100

    other pr fixes

--- front/src/views/CustomPages/PullRequests.vue
+++ front/src/views/CustomPages/PullRequests.vue
@@ -47,7 +47,9 @@
             <div class="bg-white rounded-lg shadow p-6">
                 <div class="flex items-center">
                     <div class="p-2 bg-blue-100 rounded-lg">
-                        <i class="pi pi-git-merge text-blue-600 text-xl"></i>
+                        <svg class="octicon octicon-git-pull-request text-blue-600" width="20" height="20" viewBox="0 0 16 16" fill="currentColor">
+                            <path d="M7.177 3.073L9.573.677A.25.25 0 0110 .854v4.792a.25.25 0 01-.427.177L7.177 3.427a.25.25 0 010-.354zM3.75 2.5a.75.75 0 100 1.5.75.75 0 000-1.5zm-2.25.75a2.25 2.25 0 113 2.122v5.256a2.251 2.251 0 11-1.5 0V5.372A2.25 2.25 0 011.5 3.25zM11 2.5h-1V4h1a1 1 0 011 1v5.628a2.251 2.251 0 101.5 0V5A2.5 2.5 0 0011 2.5zm1 10.25a.75.75 0 111.5 0 .75.75 0 01-1.5 0zM3.75 12a.75.75 0 100 1.5.75.75 0 000-1.5z"></path>
+                        </svg>
                     </div>
                     <div class="ml-4">
                         <p class="text-sm text-gray-600">Total PRs</p>
@@ -55,7 +57,6 @@
                     </div>
                 </div>
             </div>
-
             <div class="bg-white rounded-lg shadow p-6">
                 <div class="flex items-center">
                     <div class="p-2 bg-green-100 rounded-lg">
@@ -391,6 +392,12 @@ const stateOptions = [
     { label: 'Merged', value: 'merged' }
 ];
 
+// Computed property to find the current repository
+const repository = computed(() => {
+    if (!selectedRepositoryId.value || !repositories.value.length) return null;
+    return repositories.value.find(repo => repo.id === selectedRepositoryId.value);
+});
+
 // Utility functions
 const getAuthHeaders = () => {
     const token = localStorage.getItem('token');
@@ -594,8 +601,9 @@ onMounted(async () => {
         selectedRepositoryId.value = parseInt(repository_id);
     }
 
+    // Fetch repositories first, then statistics and pull requests
+    await fetchRepositories();
     await Promise.all([
-        fetchRepositories(),
         fetchStatistics(),
         fetchPullRequests()
     ]);
